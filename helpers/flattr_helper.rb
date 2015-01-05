2398deacb6436c85ffc2f03c1445c16f354181dc
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-01-05 22:55:04 +0200

    feat: flattr helper

---
+++ helpers/flattr_helper.rb
@@ -0,0 +1,15 @@
+module FlattrHelper
+
+  def flattr_article( article )
+    url = [ "http://www.opensourcery.co.za", article.url ].join
+
+    options = {
+      user_id: "kennethkalmer", url: url, title: article.title, language: article.lang
+    }
+
+    link_to "https://flattr.com/submit/auto", query: options, title: "Flattr this!", target: "_blank" do
+      image_tag "https://api.flattr.com/button/flattr-badge-large.png", alt: "Flattr this!"
+    end
+  end
+
+end
