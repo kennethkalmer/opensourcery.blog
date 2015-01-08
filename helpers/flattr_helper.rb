f9749bc04579f636ae5190065e7b3f6640516a8d
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-01-08 18:13:08 +0200

    feat: wrapped flattr button in a div

--- helpers/flattr_helper.rb
+++ helpers/flattr_helper.rb
@@ -7,8 +7,10 @@ module FlattrHelper
       user_id: "kennethkalmer", url: url, title: article.title, language: article.lang
     }
 
-    link_to "https://flattr.com/submit/auto", query: options, title: "Flattr this!", target: "_blank" do
-      image_tag "https://api.flattr.com/button/flattr-badge-large.png", alt: "Flattr this!"
+    content_tag 'div', class: 'flattr-button' do
+      link_to "https://flattr.com/submit/auto", query: options, title: "Flattr this!", target: "_blank" do
+        image_tag "https://api.flattr.com/button/flattr-badge-large.png", alt: "Flattr this!"
+      end
     end
   end
 
