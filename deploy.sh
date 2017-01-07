3f69d9ff916eb745bbd4b509442a9eafe73dc0c1
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2017-01-07 14:37:41 +0200

    Revert "fix: use —delete-after so old files remain deployed until the bitter end"
    This reverts commit b6a9b3f41ab27ecc2b0ad7700bf3f65b91b96bdb.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 set -ex
 
 bundle exec middleman build --clean
-rsync -e "ssh -p 222" -avz --delete-after build/ opensourcery.co.za@opensourcery.co.za:/var/www/opensourcery.co.za/
+rsync -e "ssh -p 222" -avz build/ opensourcery.co.za@opensourcery.co.za:/var/www/opensourcery.co.za/
